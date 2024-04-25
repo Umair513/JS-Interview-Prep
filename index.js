d95bac6f522713286a15d08919819a50f3e9ce35
Umair513 <soomroumair513@gmail.com>
2024-04-25 12:55:16 +0500

    split function

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-let str1 = "hello";
-let str2 = "world";
-let result = str1.concat(" ", str2)
-// first argument of substring is included but second not included
-let substring = result.substring(6,11)
-console.log(substring)
+let str = "Umair Ahmed Soomro"
+// split function splits the string into array based on the given argument called delimeter
+let result = str.split(" ");
+console.log(result)
